8ec2383681e8e97cc17076ee88fd3a3e1d7c542e
arquimvp <alberto.martinez@kairosds.com>
2019-03-20 02:51:01 -0600

    master - feature(app): Decoradores impresionantes pero anidados

--- app.ts
+++ app.ts
@@ -1,59 +1,5 @@
-//Decoradores: Es una funcion.
-//Se atepone con al prefijo "@" antes de cada funcion, propiedad, metodo, clase. 
-//No permite expandir su funcionalidad, agregandole cosas nuevas.
-//Probablemente requiera habilitar "experimentalDocrators" = true en el tsconfig.json
 
-
-//Hay 5 tipos de decaoradores:
-
-//1- De Clase:
-//Permiten interceptar al constructor de una clase, se llaman cuando se declara la clase (NO EN EL INSTANCIAMIENTO)
-//declaro mi decorador:
-
-function SelfDriving(constructorFunction: Function) {
-  console.log('-- Ya se invoco al decorador desde la definicion de la clase ;) --');
-  constructorFunction.prototype.selfDrivable = true;
-}
-
-
-//(vale la pena echarle un ojo a los prototypes en javascript)
-
-@SelfDriving
-class Developer {
-  constructor(public perfil:string){
-
-  }
-}
-
-let devIos: Developer | any = new Developer('IOS');
-console.log(`selfDriving: ${devIos['selfDrivable']}`);
-
-
-//2- Decoradores de fabrica: Se utilizan para pasar parametros.
-
-function Wheels(numOfWheels: number) {
-  console.log('-- decorador de fabrica invocado --');
-  return function (constructor: Function) {
-      console.log('-- decorador invocado --');
-      constructor.prototype.wheels = numOfWheels;
-  }
-}
-
-@Wheels(6)
-class Auto {
-  private _make: string;
-  constructor(make: string) {
-      console.log('-- constructor de clase invocado --');
-      this._make = make;
-  }
-}
-
-let carro: Auto | any = new Auto("Nissan 370z");
-console.log(carro);
-console.log(carro['wheels']);
-
-
-//3- Decoradores agregando una funcion al prototype
+//3- Decoradores anidados
 
 function avisaConTiempo(constructor:Function){
   constructor.prototype.avisa = function(){
@@ -62,6 +8,16 @@ function avisaConTiempo(constructor:Function){
 }
 
 
+
+function avisaConEnSegundoTiempo(constructor:Function){
+  constructor.prototype.avisaSegundaVez = function(){
+    console.log('esta funcion la adquiri despues de la anterior jaja!! ;)');
+    console.log(this);
+  }
+}
+
+
+@avisaConEnSegundoTiempo
 @avisaConTiempo
 class CualquierClase {
   constructor(public miNombre:string){
@@ -71,5 +27,6 @@ class CualquierClase {
 
 let avisador : CualquierClase | any = new CualquierClase('mi nombre avisador');
 avisador.avisa();
+avisador.avisaSegundaVez();
 
 
